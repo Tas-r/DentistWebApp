67e9b84e2f3d324e18af19ec9f9b4c85a1acd1d9
♡ vi ♡ <121645437+mousecarpone@users.noreply.github.com>
2025-02-28 20:06:38 -0500

    added frontend
    theyre still not linked up

---
+++ dentist_clinic/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>dental clinic!</h1>
+            <p>here lies the frontend</p>
+        </div>
+    );
+}
+
+export default App;
